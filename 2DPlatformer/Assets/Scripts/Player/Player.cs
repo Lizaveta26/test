c3e20a1c93b1995b63903537113a43bad34b8fd3
Lizaveta26 <golubival123@gmail.com>
2024-11-27 15:12:19 +0300

    Add Enemy and Double jump

--- 2DPlatformer/Assets/Scripts/Player/Player.cs
+++ 2DPlatformer/Assets/Scripts/Player/Player.cs
@@ -8,7 +8,8 @@ public class Player : MonoBehaviour
     private bool _groundCheck;
     private Vector3 _playerMovementState;
     private SpriteRenderer _sprite;
-    public int _jump; 
+    public int _jumps;
+    private int _jumpsCount; 
     private Rigidbody2D _rb2D;
     [SerializeField] private LayerMask _mask;
     Ray _ray;
@@ -25,11 +26,22 @@ public class Player : MonoBehaviour
    { 
        _sprite = GetComponentInChildren<SpriteRenderer>();
        _rb2D = GetComponent<Rigidbody2D>();
+       _jumpsCount = _jumps;
    }
 
    private void Update()
    {
-       CheckGround();
+       if (_groundCheck == true)
+       {
+           _jumpsCount = _jumps; 
+       }
+   }
+
+   void FixedUpdate()
+   {
+       _groundCheck= Physics2D.Raycast(transform.position, Vector2.down, _groundCheckRadius, _mask);
+       Debug.DrawRay(transform.position, Vector2.down * _groundCheckRadius, Color.red); 
+       
    }
 
    internal void Move(float horizontalInput)
@@ -42,16 +54,15 @@ public class Player : MonoBehaviour
 
    internal void Jump()
    {
-       _groundCheck = false;
-       _rb2D.linearVelocity = new Vector2(_rb2D.linearVelocity.x, _jumpForce);
-      
-   }
-
-   public bool CheckGround()
-   {
-       var hit = Physics2D.Raycast(transform.position, Vector2.down, _groundCheckRadius, _mask);
-      Debug.DrawRay(transform.position, Vector2.down * _groundCheckRadius, Color.red); 
-       return hit.collider != null;
+       _jumpsCount--;
+       if (_jumpsCount > 0)
+       {
+           _rb2D.linearVelocity = new Vector2(_rb2D.linearVelocity.x, _jumpForce);
+       }
+       else if (_jumpsCount == 0 && _groundCheck)
+       {
+           _rb2D.linearVelocity = new Vector2(_rb2D.linearVelocity.x, _jumpForce); 
+       }
    }
 }
  
