656e66402f83f9c980f9c429df2cbbcadcb4c353
Lizaveta26 <golubival123@gmail.com>
2024-12-14 12:10:00 +0300

    Add RandomEnemy

---
+++ SpaceShooter/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject[] spawnEnemy;
+    [SerializeField] private Transform[] spawnPoint;
+
+    public float startSpawnerInterval;
+    private float spawnerInterval;
+    
+    public int numberOfEnemies;
+    public int nowTheEnemies;
+
+    private int randEnemy;
+    private int randPoint;
+    
+    void Start()
+    {
+        spawnerInterval = startSpawnerInterval;
+    }
+    void Update()
+    {
+        if (spawnerInterval <= 0 && nowTheEnemies < numberOfEnemies)
+        {
+            randEnemy = Random.Range(0, spawnEnemy.Length);
+            randPoint = Random.Range(0, spawnPoint.Length);
+            
+            Instantiate(spawnEnemy[randEnemy], spawnPoint[randPoint].transform.position, Quaternion.identity);
+            
+            spawnerInterval = startSpawnerInterval;
+            nowTheEnemies++;
+        }
+        else
+        {
+            spawnerInterval -= Time.deltaTime;
+        }
+    }
+}
